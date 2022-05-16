268d1f52453ebf31496f599c9bdb12f4390d13d2
abodell <andybodell9@gmail.com>
2022-05-16 12:38:09 -0400

    supporting single calculations

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 function add(val1, val2) {
-    return val1 + val2;
+    let ans = val1 + val2;
+    val1 = val2;
+    val2 = '';
+    return ans;
 }
 
 function subtract(val1, val2) {
-    return val1 - val2;
+    let ans = val1  - val2;
+    val1 = val2;
+    val2 = '';
+    return ans;
 }
 
 function multiply(val1, val2) {
-    return val1 * val2;
+    let ans = val1 * val2;
+    val1 = val2;
+    val2 = '';
+    return ans;
 }
 
 function divide(val1, val2) {
-    return val1 / val2;
+    let ans = val1 / val2;
+    val1 = val2;
+    val2 = '';
+    return ans;
 }
 
+// run the correct function and then move val2 to val1 and set val2 to zero
 function operate(operator, val1, val2) {
     if (operator === '+') {
         return add(val1, val2);
@@ -28,7 +41,7 @@ function operate(operator, val1, val2) {
     }
 }
 
-function setOperator(string) {
+function setOperation(string) {
     if (string === 'X') {
         operator = '*';
     } else if (string === '+') {
@@ -37,27 +50,31 @@ function setOperator(string) {
         operator = '-';
     } else if (string === '/') {
         operator = '/'
-    } else if (string === '√') {
-        operator = '√';
     } else if (string === '%') {
         operator = '%';
     }
-    operatorHit = true;
-    console.log(operatorHit);
+
+    if (operationHit) {
+        operationHit = false;
+    } else {
+        operationHit = true;
+    }
 }
 
 let operator = '';
 let val1 = '';
 let val2 = '';
-let operatorHit = false;
+let operationHit = false;
 
 const nums = document.querySelectorAll('.btn.number');
 const operations = document.querySelectorAll('.btn.operation');
 const screen = document.querySelector('.screen');
+const equals = document.querySelector('.btn.equals');
+const operators = document.querySelectorAll('.btn.operator')
 // loop through the nodelist and create eventListener for each button
 for (let i = 0; i < nums.length; i++) {
     nums[i].addEventListener('click', (event) =>{
-        if (!operatorHit) {
+        if (!operationHit) {
             val1 += event.target.textContent;
             screen.textContent = val1;
         } else {
@@ -69,7 +86,10 @@ for (let i = 0; i < nums.length; i++) {
 // do the same thing for the operations
 for (let i = 0; i < operations.length; i++) {
     operations[i].addEventListener('click', (event) => {
-        setOperator(event.target.textContent);
-        console.log(operator);
+        setOperation(event.target.textContent);
     });
 }
+
+equals.addEventListener('click', () => {
+    screen.textContent = operate(operator, parseInt(val1), parseInt(val2));
+});
